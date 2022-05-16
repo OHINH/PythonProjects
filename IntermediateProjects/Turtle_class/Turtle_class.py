784a010a0b2a492ca1a14e5b3f8d36589ed9ee63
OHINH <olivierhinh@gmail.com>
2022-05-16 17:19:59 +0200

    Turtle Challenge n°1
    dashed lines
    polygon of different #sides

--- IntermediateProjects/Turtle_class/Turtle_class.py
+++ IntermediateProjects/Turtle_class/Turtle_class.py
@@ -1,4 +1,5 @@
 from os import terminal_size
+import random
 from turtle import Turtle as Tl, Screen
 
 Tim = Tl("arrow")
@@ -6,14 +7,25 @@ Tim.shape("turtle")
 Tim.color("teal")
 
 
-for i in range(25):
-    Tim.forward(10)
-    if i%2 == 0:
-        Tim.penup()
-    else:
-        Tim.pendown()
+def dashed_line():
+    for i in range(25):
+        Tim.forward(10)
+        if i%2 == 0:
+            Tim.penup()
+        else:
+            Tim.pendown()
 
+colors = ["black", "yellow", "red", "blue", "grey", "teal", "medium aquamarine"]
 
+def draw_a_polygon(nb_sides):
+    angle = 360/int(nb_sides)
+    for _ in range(nb_sides):
+        Tim.forward(100)
+        Tim.right(angle)
+
+for i in range(3,10):
+    Tim.color(random.choice(colors))
+    draw_a_polygon(i)
 
 
 
