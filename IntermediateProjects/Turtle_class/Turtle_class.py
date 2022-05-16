43a4ff305c54e435470c6da9b8e7b88cbc876ff1
OHINH <olivierhinh@gmail.com>
2022-05-16 09:13:38 +0200

    Turtle Challenge N°1
    Draw a square

--- IntermediateProjects/Turtle_class/Turtle_class.py
+++ IntermediateProjects/Turtle_class/Turtle_class.py
@@ -1,2 +1,29 @@
-from turtle import Turtle
+from os import terminal_size
+from turtle import Turtle as Tl, Screen
+
+Tim = Tl("arrow")
+Tim.shape("turtle")
+Tim.color("teal")
+
+
+for i in range(4):
+    Tim.forward(50)
+    Tim.rt(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
 
