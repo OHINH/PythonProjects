8f63ef7009b91eb726f81d2db92a2f906ab3ea20
OHINH <olivierhinh@gmail.com>
2022-05-21 09:51:20 +0200

    Update Snake_game
    added move snake and control snake

--- IntermediateProjects/Turtle - 4. Snake_game/snake_game.py
+++ IntermediateProjects/Turtle - 4. Snake_game/snake_game.py
@@ -1,36 +1,39 @@
 from turtle import Turtle, Screen, time
+from snake import Snake
 import random
+
+t = Turtle()
 
 # setup screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Welcome to the snake game!")
-screen.tracer(0)
+screen.tracer(False)
 
 # create a snake body, aka three white turtles
-starting_pos = [(0,0), (-20,0), (-40,0)]
-segments = []
-for position in starting_pos:
-    new_seg = Turtle("square")
-    new_seg.color("white")
-    new_seg.penup()
-    new_seg.goto(position)
-    segments.append(new_seg)
+snake = Snake()
+
+# control the snake
+screen.listen()
+screen.onkeypress(fun=snake.o, key="o")
+screen.onkeypress(fun=snake.k, key="k")
+screen.onkeypress(fun=snake.l, key="l")
+screen.onkeypress(fun=snake.m, key="m")
 
 # move the snake
 game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor
-        new_y = segments[seg_num - 1].ycor
-        segments[seg_num].goto(new_x, new_y)
-        
+while game_is_on: 
+    screen.update()
+    snake.move()  
+    time.sleep(0.1)  
+
+screen.mainloop()
+
 
 # create snake food
+
 
 # detect collision with food
 
