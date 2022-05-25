29791cc40e356cef2864bb0d649d972bc65420b0
OHINH <olivierhinh@gmail.com>
2022-05-25 18:46:01 +0200

    End of Pong
    Finished alone the PONG game, without help!

--- IntermediateProjects/Turtle - 5. Pong Game/PongGame.py
+++ IntermediateProjects/Turtle - 5. Pong Game/PongGame.py
@@ -18,8 +18,13 @@ referee = Ref()
 ## moving 
 paddle_right = Paddle(position=(350,0))
 paddle_left = Paddle(position=(-350,0))
+ball = Ball()
+left_scoreboard = Scoreboard(-40)
+right_scoreboard = Scoreboard(40)
+
 
 screen.listen()
+
 screen.onkey(fun=paddle_left.move_up, key="e")
 screen.onkey(fun=paddle_left.move_down, key="d")
 screen.onkey(fun=paddle_right.move_up, key="o")
@@ -27,7 +32,26 @@ screen.onkey(fun=paddle_right.move_down, key="l")
 
 game_is_on = True
 while game_is_on:
+    ball.move()
     screen.update()
+    time.sleep(0.1)
+
+    if ball.ycor() < -285 or ball.ycor() > 285:
+        ball.bounce() 
+    
+    if (ball.distance(paddle_left) < 35 and ball.xcor() < -325) or (ball.distance(paddle_right) < 35 and ball.xcor() > 325):
+        ball.bounce_on_paddle()
+    
+    if ball.xcor() > 380:
+        left_scoreboard.score += 1
+        left_scoreboard.sc_refresh()
+        ball.recenter()
+    if ball.xcor() < -380:
+        right_scoreboard.score += 1
+        right_scoreboard.sc_refresh()
+        ball.recenter()
+    
+    
     
 # moving ball
 ## same moving as platform, but also on x axis
--- IntermediateProjects/Turtle - 5. Pong Game/objects.py
+++ IntermediateProjects/Turtle - 5. Pong Game/objects.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+import random
+GAME_COLOR = "green"
 
 class Paddle(Turtle):
     def __init__(self, position):
@@ -6,38 +8,82 @@ class Paddle(Turtle):
         self.penup()
         self.shape("square")
         self.goto(position)
-        self.color("white")
+        self.color(GAME_COLOR)
         self.shapesize(4, 1)
     
     def move_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() < 230:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)
 
     def move_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
-
-class Ball(Turtle):
-    def __init__(self) -> None:
-        super().
-
-class Scoreboard():
-    pass
+        if self.ycor() > -230:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y)
 
 class Ref():
     def __init__(self) -> None:
         self = Turtle()
         self.hideturtle()
-        self.pencolor("white")
+        self.pencolor(GAME_COLOR)
         self.pensize(5)
         self.penup()
-        self.goto(0, 230)
+        self.goto(0, 300)
         self.setheading(270)
-        for _ in range(17):
+        for _ in range(19):
             self.forward(15)
             self.penup()
             self.forward(15)
             self.pendown()   
+
+
+
+
+class Ball(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(0.8, 0.8)
+        self.color(GAME_COLOR)
+        self.penup()
+        self.x_pas = 10
+        self.y_pas = 10
     
+    def move(self):
+        new_x = self.xcor() + self.x_pas
+        new_y = self.ycor() + self.y_pas
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_pas *= -1
+    
+    def bounce_on_paddle(self):
+        self.x_pas *= -1
+    
+    def recenter(self):
+        self.goto(0,0)
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.score = 0
+        self.goto(position, 210)
+        self.hideturtle()
+        self.color(GAME_COLOR)
+        self.sc_refresh()
+    
+    def sc_refresh(self):
+        self.clear()
+        self.write(arg=f"{self.score}", move=False, align="center", font=("Georgia", 60, "bold"))
+    
+    def game_over(self):
+        self.clear()
+        self.write(arg=f"GAME OVER. Final score: {self.score}.", align="center", font=("Georgia", 60, "bold"))
+    
+
+
+
 
 
