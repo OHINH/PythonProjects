56ccd14abf9ae6610dee2cf45e93f363132795f7
OHINH <olivierhinh@gmail.com>
2022-06-04 22:01:44 +0200

    modification w/ list comprehension

--- IntermediateProjects/Panda/US States guessing game/main.py
+++ IntermediateProjects/Panda/US States guessing game/main.py
@@ -14,19 +14,20 @@ writer.hideturtle()
 
 data = pandas.read_csv("50_states.csv")
 states = data.state.to_list()
-not_guessed_states = states
+guessed_states = []
 
 
-while len(not_guessed_states) > 0:
-    answer_state = screen.textinput(title=f"Guess the State - {len(not_guessed_states)}/50", prompt="Type in a State name.").title()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"Guess the State - {len(guessed_states)}/50", prompt="Type in a State name.").title()
     if answer_state in states:
         writer.goto(int(data[answer_state == data.state].x), int(data[answer_state == data.state].y))
         writer.write(f"{answer_state}")
-        not_guessed_states.remove(answer_state)
+        guessed_states.append(answer_state)
     
     if answer_state == "Exit":
-        print(f"The States you haven't guessed are: {not_guessed_states}")
-        new_data = pandas.DataFrame(not_guessed_states)
+        missing_states = [state for state in states if state not in guessed_states]
+        print(f"The States you haven't guessed are: {missing_states}")
+        new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("missing_states.csv")
         break
         
